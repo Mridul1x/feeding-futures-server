fe5fd8795d11ba680601411a2f6e88076794aa8d
Mridul1x <mridulhasannn@gmail.com>
2023-06-25 15:54:32 +0600

    Server file set-up

---
+++ server.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome to Feeding Futures server!" });
+});
+
+const port = process.env.PORT || 8080;
+const uri = process.env.MONGO_URI;
+
+mongoose
+  .connect(uri, { useUnifiedTopology: true })
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server running on port : ${port}`);
+    });
+  })
+  .catch(() => {
+    console.log(err);
+  });
